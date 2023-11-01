51c39d624b40a499fb4ee846a83431b88efd8e77
Ahonsi Julia <ahonsijulia@gmail.com>
2023-11-01 12:58:58 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+
+const Navbar = () => {
+    return (
+      <div>
+        <div className="">
+          <hi className="text-red-600 text-4xl font-bold">NETFLIX</hi>
+        </div>
+        <div>
+            <button>Sign In</button>
+            <button>Sign Up</button>
+        </div>
+      </div>
+    );
+}
+
+export default Navbar
